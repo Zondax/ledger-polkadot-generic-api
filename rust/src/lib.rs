8476c0863671fb104d056e0efbf827b9f3d1a821
emmanuelm41 <emmanuelm41@gmail.com>
2024-04-19 13:29:20 -0300

    fix: extrinsic metadata field

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,100 +1,139 @@
 use frame_metadata::v15::RuntimeMetadataV15;
-use metadata_shortener::{
-    cut_metadata_transaction_unmarked,
-    traits::{Blake3Leaf, HashableMetadata},
-    ShortSpecs,
+use frame_metadata::{RuntimeMetadata, RuntimeMetadataPrefixed};
+use merkleized_metadata::{
+  generate_metadata_digest, generate_proof_for_extrinsic, ExtraInfo, ExtrinsicMetadata,
+  FrameMetadataPrepared, Proof,
 };
 use neon::prelude::*;
 use parity_scale_codec::{Decode, Encode};
 
-fn get_specs<'a>(
-    cx: &mut impl Context<'a>,
-    js_props: Handle<'a, JsObject>,
-) -> NeonResult<ShortSpecs> {
-    let base58prefix = js_props
-        .get_value(cx, "base58prefix")?
-        .downcast::<JsNumber, _>(cx)
-        .unwrap()
-        .value(cx) as u16;
-    let decimals = js_props
-        .get_value(cx, "decimals")?
-        .downcast::<JsNumber, _>(cx)
-        .unwrap()
-        .value(cx) as u8;
-    let unit = js_props
-        .get_value(cx, "unit")?
-        .downcast::<JsString, _>(cx)
-        .unwrap()
-        .value(cx);
-    Ok(ShortSpecs {
-        base58prefix,
-        decimals,
-        unit,
-    })
+#[derive(Encode)]
+pub struct MetadataProof {
+  proof: Proof,
+  extrinsic: ExtrinsicMetadata,
+  extra_info: ExtraInfo,
+}
+
+fn get_extra_info<'a>(
+  cx: &mut impl Context<'a>,
+  js_props: Handle<'a, JsObject>,
+) -> NeonResult<ExtraInfo> {
+  let base58_prefix = js_props
+    .get_value(cx, "base58Prefix")?
+    .downcast::<JsNumber, _>(cx)
+    .unwrap()
+    .value(cx) as u16;
+  let decimals = js_props
+    .get_value(cx, "decimals")?
+    .downcast::<JsNumber, _>(cx)
+    .unwrap()
+    .value(cx) as u8;
+  let token_symbol = js_props
+    .get_value(cx, "tokenSymbol")?
+    .downcast::<JsString, _>(cx)
+    .unwrap()
+    .value(cx);
+  let spec_name = js_props
+    .get_value(cx, "specName")?
+    .downcast::<JsString, _>(cx)
+    .unwrap()
+    .value(cx);
+  let spec_version = js_props
+    .get_value(cx, "specVersion")?
+    .downcast::<JsNumber, _>(cx)
+    .unwrap()
+    .value(cx) as u32;
+  Ok(ExtraInfo {
+    base58_prefix,
+    decimals,
+    token_symbol,
+    spec_name,
+    spec_version,
+  })
 }
 
 fn get_short_metadata(mut cx: FunctionContext) -> JsResult<JsString> {
-    let param_obj = cx.argument::<JsObject>(0).unwrap();
-    let blob_str = param_obj
-        .get_value(&mut cx, "blob")?
-        .downcast::<JsString, _>(&mut cx)
-        .unwrap()
-        .value(&mut cx);
-    let metadata_str = param_obj
-        .get_value(&mut cx, "metadata")?
-        .downcast::<JsString, _>(&mut cx)
-        .unwrap()
-        .value(&mut cx);
-    let js_props = param_obj
-        .get_value(&mut cx, "props")?
-        .downcast::<JsObject, _>(&mut cx)
-        .unwrap();
-    let specs = get_specs(&mut cx, js_props)?;
-    let blob = hex::decode(blob_str).unwrap();
-    let metadata = hex::decode(metadata_str).unwrap();
+  let param_obj = cx.argument::<JsObject>(0).unwrap();
+  let call_data_str = param_obj
+    .get_value(&mut cx, "callData")?
+    .downcast::<JsString, _>(&mut cx)
+    .unwrap()
+    .value(&mut cx);
+  let sig_ext_str = param_obj
+    .get_value(&mut cx, "signedExtensions")?
+    .downcast::<JsString, _>(&mut cx)
+    .unwrap()
+    .value(&mut cx);
+  let metadata_str = param_obj
+    .get_value(&mut cx, "metadata")?
+    .downcast::<JsString, _>(&mut cx)
+    .unwrap()
+    .value(&mut cx);
+  let js_props = param_obj
+    .get_value(&mut cx, "props")?
+    .downcast::<JsObject, _>(&mut cx)
+    .unwrap();
+  let specs = get_extra_info(&mut cx, js_props)?;
+  // we need to append two bytes
+  //   - 0 stands for a "length". It's not used at all, that's why we only add a 0 (it's a
+  //     Compact<u32> theoretically)
+  //   - 4 stands for extrinsicVersion 4. We don't fully need it, but the shortener requires it
+  let call_data = [Vec::<u8>::from([0, 4]), hex::decode(call_data_str).unwrap()].concat();
+  let sig_ext = hex::decode(sig_ext_str).unwrap();
+  let metadata = hex::decode(metadata_str).unwrap();
 
-    let meta_v15 = RuntimeMetadataV15::decode(&mut &metadata[5..]).unwrap();
-    let short_metadata_str = match cut_metadata_transaction_unmarked::<_, _, Blake3Leaf, _>(
-        &blob.as_ref(),
-        &mut (),
-        &meta_v15,
-        &specs,
-    ) {
-        Ok(x) => hex::encode(x.encode()),
-        Err(_) => String::from(""),
+  let runtime_meta_v15 = RuntimeMetadataV15::decode(&mut &metadata[5..]).unwrap();
+  let runtime_meta = RuntimeMetadata::V15(runtime_meta_v15);
+  let registry_proof =
+    match generate_proof_for_extrinsic(&call_data, Some(&sig_ext), &runtime_meta) {
+      Ok(x) => x,
+      Err(x) => return Ok(cx.string(x)),
     };
-    Ok(cx.string(short_metadata_str))
+
+  // Generates extrinsic_metadata in the same way the crate does
+  let extrinsic_metadata = FrameMetadataPrepared::prepare(
+    &RuntimeMetadataPrefixed::decode(&mut &metadata[..])
+      .unwrap()
+      .1,
+  )
+    .unwrap()
+    .as_type_information()
+    .extrinsic_metadata;
+
+  let meta_proof = MetadataProof {
+    proof: registry_proof,
+    extrinsic: extrinsic_metadata,
+    extra_info: specs,
+  };
+  Ok(cx.string(hex::encode(meta_proof.encode())))
 }
 
 fn get_metadata_digest(mut cx: FunctionContext) -> JsResult<JsString> {
-    let param_obj = cx.argument::<JsObject>(0).unwrap();
-    let metadata_str = param_obj
-        .get_value(&mut cx, "metadata")?
-        .downcast::<JsString, _>(&mut cx)
-        .unwrap()
-        .value(&mut cx);
-    let js_props = param_obj
-        .get_value(&mut cx, "props")?
-        .downcast::<JsObject, _>(&mut cx)
-        .unwrap();
-    let specs = get_specs(&mut cx, js_props)?;
-    let metadata = hex::decode(metadata_str).unwrap();
+  let param_obj = cx.argument::<JsObject>(0).unwrap();
+  let metadata_str = param_obj
+    .get_value(&mut cx, "metadata")?
+    .downcast::<JsString, _>(&mut cx)
+    .unwrap()
+    .value(&mut cx);
+  let js_props = param_obj
+    .get_value(&mut cx, "props")?
+    .downcast::<JsObject, _>(&mut cx)
+    .unwrap();
+  let extra_info = get_extra_info(&mut cx, js_props)?;
+  let metadata = hex::decode(metadata_str).unwrap();
+  let runtime_meta_v15 = RuntimeMetadataV15::decode(&mut &metadata[5..]).unwrap();
+  let runtime_meta = RuntimeMetadata::V15(runtime_meta_v15);
 
-    let meta_v15 = RuntimeMetadataV15::decode(&mut &metadata[5..]).unwrap();
-    let digest = match <RuntimeMetadataV15 as HashableMetadata<()>>::digest_with_short_specs(
-        &meta_v15,
-        &specs,
-        &mut (),
-    ) {
-        Ok(x) => hex::encode(x.encode()),
-        Err(_) => String::from(""),
-    };
-    Ok(cx.string(digest))
+  let digest = match generate_metadata_digest(&runtime_meta, extra_info) {
+    Ok(x) => hex::encode(x.hash()),
+    Err(x) => x,
+  };
+  Ok(cx.string(digest))
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("getShortMetadata", get_short_metadata)?;
-    cx.export_function("getMetadataDigest", get_metadata_digest)?;
-    Ok(())
+  cx.export_function("getShortMetadata", get_short_metadata)?;
+  cx.export_function("getMetadataDigest", get_metadata_digest)?;
+  Ok(())
 }
