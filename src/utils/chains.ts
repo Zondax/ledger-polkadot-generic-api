52c996f3814f27573db22cb28d67c918aa6f1455
emmanuelm41 <emmanuelm41@gmail.com>
2024-04-19 14:46:32 -0300

    fix lint

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -19,12 +19,17 @@ export function loadChains(filePath: string) {
   const file = fs.readFileSync(filePath, 'utf8')
   const yamlFile = yaml.parse(file)
 
-  if (!yamlFile['chains']) throw new Error('invalid chains yaml')
-  if (!(yamlFile['chains'] instanceof Array)) throw new Error('invalid chains yaml')
+  if (!yamlFile['chains']) {
+    throw new Error('invalid chains yaml, first field must be "chains"')
+  }
+  if (!(yamlFile['chains'] instanceof Array)) {
+    throw new Error('invalid chains yaml, chains should be array')
+  }
   if (
     !yamlFile['chains'].reduce((isValid, chain) => typeof chain == 'object' && !!chain.id && !!chain.name && !!chain.url && isValid, true)
-  )
-    throw new Error('invalid chains yaml')
+  ) {
+    throw new Error('invalid chains yaml, chain elements should have id, name and url')
+  }
 
   return yamlFile as ChainsFile
 }
